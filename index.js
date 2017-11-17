febe220e3c526a28b8a284c46810487ec2b15670
fisker <lionkay@gmail.com>
2017-11-17 14:56:54 +0800

    update

--- index.js
+++ index.js
@@ -3,49 +3,52 @@
  * fisker Cheung<lionkay@gmail.com>
  */
 
-var HTMLHint = require('htmlhint').HTMLHint;
-var log = (global.fis && fis.log) || console;
+var HTMLHint = require('htmlhint').HTMLHint
+var log = global.fis.log
 
 function readConfig(filename) {
-  var fs = require('fs');
-  var path = require('path');
-  var currentFolder = process.cwd();
-  var currentFile;
-  var parentFolder;
+  var fs = require('fs')
+  var path = require('path')
+  var currentFolder = process.cwd()
+  var currentFile
+  var parentFolder
 
   do {
-    currentFolder = parentFolder || currentFolder;
-    currentFile = path.normalize(path.join(currentFolder, filename));
+    currentFolder = parentFolder || currentFolder
+    currentFile = path.normalize(path.join(currentFolder, filename))
 
     if (fs.existsSync(currentFile)) {
       try {
-        return JSON.parse(require('fs').readFileSync(currentFile, 'utf8'));
-      } catch(_) {
-        return;
+        return JSON.parse(require('fs').readFileSync(currentFile, 'utf8'))
+      } catch (_) {
+        return
       }
     }
 
-    parentFolder = path.resolve(currentFolder, '../');
+    parentFolder = path.resolve(currentFolder, '../')
   } while (parentFolder !== currentFolder)
 }
 
-var htmlhintrcConfig;
+var htmlhintrcConfig
 
-module.exports = function(content, file, conf){
+module.exports = function(content, file, conf) {
   if (!content) {
-    return;
+    return
   }
 
-  var ruleset = conf.rules || htmlhintrcConfig || (htmlhintrcConfig = readConfig('.htmlhintrc') || {});
+  var ruleset =
+    conf.rules ||
+    htmlhintrcConfig ||
+    (htmlhintrcConfig = readConfig('.htmlhintrc') || {})
 
-  var results = HTMLHint.verify(content, ruleset);
-  var errorType = 'warning';
+  var results = HTMLHint.verify(content, ruleset)
+  var errorType = 'warning'
 
   results.forEach(function(msg) {
     if (msg.type === 'error') {
-      errorType = 'error';
+      errorType = 'error'
     }
-  });
+  })
 
   if (results.length) {
     log.warn(
@@ -53,9 +56,9 @@ module.exports = function(content, file, conf){
       file.id,
       errorType,
       HTMLHint.format(results, {indent: 2}).join('\n')
-    );
+    )
     if (errorType === 'error') {
-      process.exit(1);
+      process.exit(1)
     }
   }
-};
+}
